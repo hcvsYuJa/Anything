9b79de1331e6a1a3b25b00f29dff68ab36931ca2
hcvsYuJa <48265002+hcvsYuJa@users.noreply.github.com>
2019-03-21 08:00:26 +0800

    Update 2^7.c

--- 2/2^7.c
+++ 2/2^7.c
@@ -3,6 +3,7 @@ int main ()
 {
 	double a1=0,b1=0,c=1;
 	long long int a2=0,b2=0;
+	char zero = '0';
 	char A[15]={'\0'};
 	scanf("%lf%lf",&a1,&b1);
 	for(;(long long int)a1<a1||(long long int)b1<b1;c=c*0.1)
@@ -22,6 +23,10 @@ int main ()
 	b2=0;
 	for(a2=14;a2>=0;a2=a2-1)
 	{
+		if(A[a2]=='0'||A[a2]=='.')zero='1';
+		else
+		zero='0';
+		if(A[a2]=='.'&&zero=='1')A[a2]='\0';
 		if(A[a2]=='.')continue;
 		if('1'<=A[a2]&&A[a2]<='9')b2=1;
 		else if(b2==0&&A[a2]=='0')A[a2]='\0';
